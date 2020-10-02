227b690ae17968c25d8546093bd22f91b616c03d
Delmy Melendez <delmymm93@gmail.com>
2020-10-01 20:55:37 -0600

    code added to existing JS file.

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var symbols = ["!", "@", "#", "$", "%", "^", "*", "(", ")", "_", "+"]
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = prompt("How many characters do you want your password to contain?");
+  var specialCharacters = confirm("Click OK to confirm special characters");
+  var numbers = confirm("Click OK to add numbers");
+  var lowerCase = confirm("Click OK to add lowercase characters");
+  var upperCase = confirm("Click OK to add uppercase characters");
+
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+function generatePassword(params) {
+  var passwordArray = [];
+  var selectedFunctions = [];
+  if (lowerCase) {
+    selectedFunctions.push(generateLowerCase)
+  }
+  if (upperCase) {
+    selectedFunctions.push(generateUpperCase)
+  }
+  if (numbers) {
+    selectedFunctions.push(generateNumbers)
+  }
+  if (specialCharacters) {
+    selectedFunctions.push(generateCharacters)
+  }
+  for (let index = 0; index < passwordLength; index++) {
+    const element = array[index];
 
+  }
+}
+function generateLowerCase(params) {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function generateUpperCase(params) {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function generateNumbers(params) {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function generateSpecialCharacters(params) {
+  return symbol[Math.floor(Math.random()*symbol.length)];
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
