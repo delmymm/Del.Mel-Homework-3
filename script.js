c4deec2e89e7a06d6545e7a6bd5423f81ecca6d2
Delmy Melendez <delmymm93@gmail.com>
2020-10-02 22:26:12 -0600

    Restarted java code.

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var symbols = ["!", "@", "#", "$", "%", "^", "*", "(", ")", "_", "+"]
+
 // Write password to the #password input
 function writePassword() {
   var passwordLength = prompt("How many characters do you want your password to contain?");
+  if (passwordLength < 8 ) {
+    alert("Need more characters!")
+  }
+  if (passwordLength > 125 ) {
+    alert("Exceeded character length!")
+  }
   var specialCharacters = confirm("Click OK to confirm special characters");
+  if (specialCharacters) {
+    selectedFunctions.push(generateSpecialCharacters)
+  }
   var numbers = confirm("Click OK to add numbers");
+  if (numbers) {
+    selectedFunctions.push(generateNumbers)
+  }
   var lowerCase = confirm("Click OK to add lowercase characters");
+  if (lowerCase) {
+    selectedFunctions.push(generateLowerCase)
+  }
   var upperCase = confirm("Click OK to add uppercase characters");
+  if (upperCase) {
+    selectedFunctions.push(generateUpperCase)
+  }
 
   var password = generatePassword();
 
@@ -16,45 +34,24 @@ function writePassword() {
 
   passwordText.value = password;
 }
-function generatePassword(params) {
-  var passwordArray = [];
-  var selectedFunctions = [];
-  if (lowerCase) {
-    selectedFunctions.push(generateLowerCase)
-  }
-  if (upperCase) {
-    selectedFunctions.push(generateUpperCase)
-  }
-  if (numbers) {
-    selectedFunctions.push(generateNumbers)
-  }
-  if (numbers > 8 ) {
-    count --;
-  }
-  if (numbers < 125 ) {
-    count ++;
-  }
-  if (specialCharacters) {
-    selectedFunctions.push(generateCharacters)
-    document.body.appendChild(writePassword);
-  }
-  for (var index = 0; index < passwordLength; index++) {
-    console.log(array[index]);
 
-  }
+function generatePassword() {
 }
-function generateLowerCase(params) {
+
+function generateLowerCase(_params) {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-function generateUpperCase(params) {
+function generateUpperCase(_params) {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-function generateNumbers(params) {
+function generateNumbers(_params) {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-function generateSpecialCharacters(params) {
-  return symbol[Math.floor(Math.random()*symbol.length)];
+function generateSpecialCharacters(_params) {
+  return symbols[Math.floor(Math.random()*symbols.length)];
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
